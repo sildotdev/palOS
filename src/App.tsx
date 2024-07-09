522c8fd4a3ccca8d6d5498a2f65f489e16124aa5
sildotdev <git@sil.dev>
2024-07-08 21:08:31 -0500

    works on web

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { List, TaskBar, Modal } from '@react95/core';
+import { ReaderClosed, WindowsExplorer } from '@react95/icons';
 import './App.css';
 
+// import '@react95/core/GlobalStyle';
+// import '@react95/core/themes/win95.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [first, toggleFirst] = useState(false);
+  const [second, toggleSecond] = useState(false);
+  const closeFirst = () => toggleFirst(false);
+  const closeSecond = () => toggleSecond(false);
+  return <>
+      {first && <Modal icon={<WindowsExplorer variant="16x16_4" />} title="Windows Explorer" closeModal={closeFirst} width="300px" height="200px" />}
+
+      {second && <Modal defaultPosition={{
+      x: 50,
+      y: 50
+    }} width="300px" height="200px" icon={<ReaderClosed variant="16x16_4" />} title="Local Disk (C:)" closeModal={closeSecond} />}
+
+      <TaskBar list={<List>
+            <List.Item icon={<ReaderClosed variant="32x32_4" />} onClick={() => toggleSecond(true)}>
+              Local Disk (C:)
+            </List.Item>
+            <List.Item icon={<WindowsExplorer variant="32x32_4" />} onClick={() => {
+        toggleFirst(true);
+      }}>
+              Windows Explorer
+            </List.Item>
+          </List>} />
+    </>;
 }
 
 export default App;
